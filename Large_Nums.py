a2cfe8560c6cc724d16201f413c29a3ebfb5db96
Justin Goodwyn <justingoodwyn@Mandys-MacBook-Air.local>
2017-12-19 19:28:29 -0600

    Copied code from repl

---
+++ Large_Nums.py
@@ -0,0 +1,33 @@
+    #def jimmie(a,b):
+    #echo "# Pointless_Forever_Loops" >> README.md
+    #git init
+    #git add README.md
+    #git commit -m "first commit"
+    #git remote add origin https://github.com/JustinGoodwyn/Pointless_Forever_Loops.git
+    #git push -u origin master
+a = input("choose forever loop one or two (number two may lag... a lot)")
+if a == ("1"):
+    x = 2
+    while x == x:
+        x += x
+        print(x)
+if a == ("2"):
+    x = 2
+    while x == x:
+        x *= x
+        print(x)
+#	"https://repl.it/@JustinGoodwyn/Pointless-forever-loops-that-make-large-numbers"
+if a == ("3"):
+    x = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    while x ==x:
+        x += [1]
+        print(x)
+if a == ("4"):
+    x = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    while x ==x:
+        x += x
+        print(x)
+
+
+
+a
